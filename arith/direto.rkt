7b0368ac46e5b43d77e9ce5881f624574fba02df
danigfavero <danifavero@usp.br>
2019-09-02 22:12:45 -0300

    if no racket

--- arith/direto.rkt
+++ arith/direto.rkt
@@ -4,7 +4,8 @@
   [numC (n : number)]
   [plusC (l : ArithC) (r : ArithC)]
   [multC (l : ArithC) (r : ArithC)]
-  [divC (l : ArithC) (r : ArithC)])
+  [divC (l : ArithC) (r : ArithC)]
+  [ifC   (condition : ArithC) (yes : ArithC) (no : ArithC)])
 
 (define-type ArithS
   [numS    (n : number)]
@@ -12,7 +13,8 @@
   [bminusS (l : ArithS) (r : ArithS)]
   [uminusS (e : ArithS)]
   [multS   (l : ArithS) (r : ArithS)]
-  [divS (l : ArithS) (r : ArithS)]))
+  [divS    (l : ArithS) (r : ArithS)]
+  [ifS     (c : ArithS) (s : ArithS) (n : ArithS)])
 
 
 (define (desugar [as : ArithS]) : ArithC  
@@ -23,6 +25,7 @@
     [divS    (l r) (divC (desugar l) (desugar r))]
     [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
     [uminusS (e)   (multC (numC -1) (desugar e))]
+    [ifS     (c s n) (ifC (desugar c) (desugar s) (desugar n))]
     ))
 
 
@@ -31,7 +34,10 @@
     [numC (n) n]
     [plusC (l r) (+ (interp l) (interp r))]
     [multC (l r) (* (interp l) (interp r))]
-    [divC (l r)  (/ (interp l) (interp r))]))
+    [divC (l r)  (/ (interp l) (interp r))]
+    [ifC (c s n) (if (zero? (interp c fds)) (interp n fds) (interp s fds))]
+  )
+)
 
 
 (define (parse [s : s-expression]) : ArithS
@@ -42,10 +48,11 @@
        (case (s-exp->symbol (first sl))
          [(+) (plusS (parse (second sl)) (parse (third sl)))]
          [(*) (multS (parse (second sl)) (parse (third sl)))]
-         [(*) (divS  (parse (second sl)) (parse (third sl)))]
+         [(/) (divS  (parse (second sl)) (parse (third sl)))]
          [(-) (bminusS (parse (second sl)) (parse (third sl)))]
          ; para o parser precisamos um sinal negativo...
          [(~) (uminusS (parse (second sl)))]
+         [(if) (ifS (parse (second sl)) (parse (third sl)) (parse (fourth sl)))]
          [else (error 'parse "invalid list input")]))]
     [else (error 'parse "invalid input")]))
 
