cbbe65d91726e4f2d83ff051239d1263876dae1b
danigfavero <danifavero@usp.br>
2019-10-12 17:29:03 -0300

    interpretador funciona

--- interp.rkt
+++ interp.rkt
@@ -6,10 +6,6 @@
   [plusC (l : ExprC) (r : ExprC)]
   [multC (l : ExprC) (r : ExprC)]
   [divC  (l : ExprC) (r : ExprC)]
-  [andC  (l : ExprC) (r : ExprC)]
-  [orC   (l : ExprC) (r : ExprC)]
-  [eqC   (l : ExprC) (r : ExprC)]
-  [gtC   (l : ExprC) (r : ExprC)]
   [ifC   (condition : ExprC) (yes : ExprC) (no : ExprC)]
   [lamC (arg : symbol) (body : ExprC)]
   [appC (fun : ExprC) (arg : ExprC)]
@@ -28,10 +24,6 @@
   [uminusS (e : ExprS)]
   [multS   (l : ExprS) (r : ExprS)]
   [divS    (l : ExprS) (r : ExprS)]
-  [andS    (l : ExprS) (r : ExprS)]
-  [orS     (l : ExprS) (r : ExprS)]
-  [eqS     (l : ExprS) (r : ExprS)]
-  [gtS     (l : ExprS) (r : ExprS)]
   [ifS     (c : ExprS) (y : ExprS) (n : ExprS)]
   [setS    (var : symbol) (arg : ExprS)]
   [seqS    (b1 : ExprS) (b2 : ExprS)]
@@ -48,16 +40,12 @@
     [plusS   (l r) (plusC (desugar l) (desugar r))] 
     [multS   (l r) (multC (desugar l) (desugar r))]
     [divS    (l r) (divC (desugar l) (desugar r))]
-	  [andS  	 (l r) (andC  (desugar l) (desugar r))]
-	  [orS   	 (l r) (orC   (desugar l) (desugar r))]
-	  [eqS   	 (l r) (eqC   (desugar l) (desugar r))]
-	  [gtS   	 (l r) (gtC   (desugar l) (desugar r))]
     [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
     [uminusS (e)   (multC (numC -1) (desugar e))]
     [ifS     (c y n) (ifC (desugar c) (desugar y) (desugar n))]
     [setS    (s v)   (setC s (desugar v))]
     [seqS    (b1 b2) (seqC (desugar b1) (desugar b2))]
-    [letS    (id : symbol) (val : ExprS) (body : ExprS)]
+    [letS    (id val expr) (appC  (lamC id (desugar expr)) (desugar val))]
   )
 )
 
@@ -173,15 +161,13 @@
 (define-type Result
       [v*s (v : Value) (s : Store)])
 
-;; FIXME que isso??????????
-(define (b->n n) (if n 1 0))
-(define (n->b n) : boolean (if (zero? n) #f #t))
+
 
 ; Agora o interpretador
 
 (define (interp [a : ExprC] [env : Env] [sto : Store]) : Result
   (type-case ExprC a
-    [numC (n) n (v*s (numV n) sto)]
+    [numC (n) (v*s (numV n) sto)] 
     [varC (n)  (v*s (fetch (lookup n env) sto) sto)]  ; busca em cascata, env e em seguida no sto
     [lamC (a b) (v*s (closV a b env) sto)]
     [seqC (b1 b2) (type-case Result (interp b1 env sto)
@@ -191,9 +177,9 @@
                   )
     ]
     ; aplicação de função
-    [appC (f a) 
+    [appC (f a)
       (type-case Result (interp f env sto) ; acha a função
-          [v*s (v-f s-f)
+         [v*s (v-f s-f)
               (type-case Result (interp a env s-f) ; argumento com sto modificado pela função
                  [v*s (v-a s-a)
                       (let ([onde (new-loc)]) ; aloca posição para o valor do argumento
@@ -201,32 +187,32 @@
                                    (extend-env (bind (closV-arg v-f) onde) ; com novo argumento
                                        (closV-env v-f))
                                    (override-store (cell onde v-a) s-a))) ; com novo valor
-                  ]
+                 ]
               )
-          ]
-      )
+         ]
+        )
     ]
     [plusC (l r) 
-            (type-case Result (interp l env sto)
-                [v*s (v-l s-l)
+           (type-case Result (interp l env sto)
+               [v*s (v-l s-l)
                     (type-case Result (interp r env s-l)
                       [v*s (v-r s-r)
                            (v*s (num+ v-l v-r) s-r)
                       ]
                     )
-                ]
-            )
+               ]
+           )
     ]
     [multC (l r) 
-            (type-case Result (interp l env sto)
-                [v*s (v-l s-l)
+           (type-case Result (interp l env sto)
+               [v*s (v-l s-l)
                     (type-case Result (interp r env s-l)
                       [v*s (v-r s-r)
                            (v*s (num* v-l v-r) s-r)
                       ]
                     )
-                ]
-            )
+               ]
+           )
     ]
     [divC (l r) 
             (type-case Result (interp l env sto)
@@ -239,13 +225,10 @@
                 ]
             )
     ]
-    ; FIXME implementação do AND, OR, EQ e GT
-    ;[andC (l r)  (b->n (and (n->b (interp l fds)) (n->b (interp r fds))))]
-    ;[orC  (l r)  (b->n (or  (n->b (interp l fds)) (n->b (interp r fds))))]
-    ;[eqC  (l r)  (b->n (=   (interp l fds) (interp r fds)))]
-    ;[gtC  (l r)  (b->n (>   (interp l fds) (interp r fds)))]
     ; ifC já serializa
     [ifC (c y n) (if (zero? (numV-n (v*s-v (interp c env sto)))) (interp n env sto) (interp y env sto))]
+    
+
     [setC (var val) (type-case Result (interp val env sto)
                      [v*s (v-val s-val)
                           (let ([onde (lookup var env)]) ; acha a variável
@@ -274,7 +257,7 @@
        (case (s-exp->symbol (first sl))
          [(+) (plusS (parse (second sl)) (parse (third sl)))]
          [(*) (multS (parse (second sl)) (parse (third sl)))]
-         [(/) (divS  (parse (second sl)) (parse (third sl)))
+         [(/) (divS  (parse (second sl)) (parse (third sl)))]
          [(-) (bminusS (parse (second sl)) (parse (third sl)))]
          [(~) (uminusS (parse (second sl)))]
          [(func) (lamS (s-exp->symbol (second sl)) (parse (third sl)))] ; definição
@@ -288,17 +271,7 @@
   )
 )
 
-; FIXME arrumar facilitador e descobrir se precisa da biblioteca
+; Facilitador
 
-(define biblioteca (list 
-                    [fdC 'porcentagem 'x (divC (idC 'x) (numC 100))]
-                    [fdC 'cubo 'y ( multC (multC (idC 'y) (idC 'y)) (idC 'y))]
-                    [fdC 'milhasParaKm 'n (multC (numC 1.609) (idC 'n))]
-                    [fdC 'fahrentheitParaCelsius 't (plusC (multC (numC 1.8) (idC 't)) (numC 32))]
-                    [fdC 'fatorial 'n (ifC (gtC (idC 'n) (numC 1)) 
-                                           (multC (appC 'fatorial (plusC (idC 'n) (numC -1))) (idC 'n))
-                                           (numC 1))]
-                  )
-)
-
-(interp (desugar (parse (read))) biblioteca)
+(define (interpS [s : s-expression]) (interp (desugar (parse s)) mt-env mt-store))
+(numV-n (v*s-v (interpS (read))))
